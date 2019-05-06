572a51488b21fdc0873df63447bcaa4ca17214fe
Peter Morlion <peter.morlion@gmail.com>
2019-05-06 16:09:18 +0200

    Document calculator class

--- SimpleCalculatorLibrary/Calculator.cs
+++ SimpleCalculatorLibrary/Calculator.cs
@@ -4,6 +4,13 @@ namespace SimpleCalculatorLibrary
 {
     public class Calculator
     {
+        /// <summary>
+        /// Executes a simple mathematical operation with two integers.
+        /// </summary>
+        /// <param name="operation">The operation to execute</param>
+        /// <param name="x">The first number</param>
+        /// <param name="y">The second number</param>
+        /// <returns>An integer that is the result of the given operation.</returns>
         public int Execute(IOperation operation, int x, int y)
         {
             Console.WriteLine($"Executing {operation.GetType()} for x = {x} and y = {y}.");
